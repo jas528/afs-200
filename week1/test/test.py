0488ed5dbafe1f86c813c8b89aa7186f98109597
jas528 <76809910+jas528@users.noreply.github.com>
2021-11-26 18:57:51 -0600

    Week 1

---
+++ week1/test/test.py
@@ -0,0 +1,19 @@
+#name = input("Give me your name: ")
+#print("Your name is " + name)
+
+#age = input("Enter your age: ")
+#age = int(age)
+
+# Integers: 
+# 20 + 30 = 50
+
+# String:
+# "20" + "30" = "2030"
+
+#print("20" + "20")
+#print(20 + 20)
+
+#num1 = "20"
+#num2 = 30
+
+#print(int(num1) + int(num2))
