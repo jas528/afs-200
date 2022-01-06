0adec0e849e46f3f0b4ddefa487340dcf2259d9f
jas528 <76809910+jas528@users.noreply.github.com>
2022-01-06 12:45:20 -0600

    wk7html

--- week7/env/project/polls/views.py
+++ week7/env/project/polls/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.shortcuts import HttpResponse
 # Create your views here.
 def index (request):
-    return HttpResponse ("I have a dog name spot. He is a large dog! Spot like to play." )
+    return render(request,"index.html")
